c11da8c04fe31ed87ce5ce11b1bf6ac4c93795b3
Suddha Mukhopadhyay <suddha@bitcoin.com.au>
2019-10-19 21:39:10 +1100

    Cleanup webpack config and and dev and prod envs

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,9 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
+'use strict'
 
-module.exports = {
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
-      // this will apply to both plain `.js` files
-      // AND `<script>` blocks in `.vue` files
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-      },
-      // this will apply to both plain `.css` files
-      // AND `<style>` blocks in `.vue` files
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            },
-          },
-          'postcss-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: [
-                tailwindcss("./tailwind.js"),
-                autoprefixer({
-                  add: true,
-                  grid: true
-                }),
-              ],
-            },
-          },
-        ],
-      },
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: file => (
-          /node_modules/.test(file) &&
-          !/\.vue\.js/.test(file)
-        ),
-      },
-    ],
-  },
-  plugins: [
-    // make sure to include the plugin for the magic
-    new VueLoaderPlugin(),
-  ],
+const environment = (process.env.NODE_ENV || 'development').trim()
+
+if (environment === 'development') {
+  module.exports = require('./src/config/webpack.config.dev')
+} else {
+  module.exports = require('./src/config/webpack.config.prod')
 }
